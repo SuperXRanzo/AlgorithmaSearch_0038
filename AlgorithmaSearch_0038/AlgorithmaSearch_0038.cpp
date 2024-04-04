f9c1911e5b14c214003b8073b886a96236d2ebc4
KOMPUTER <KOMPUTER@DESKTOP-6PPP0RC>
2024-04-04 21:22:49 +0700

    Accept array element

--- AlgorithmaSearch_0038/AlgorithmaSearch_0038.cpp
+++ AlgorithmaSearch_0038/AlgorithmaSearch_0038.cpp
@@ -16,3 +16,13 @@ void input()
 		else
 			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+	// Accept array element
+	cout << "\n-------------------\n";
+	cout << " Enter array element \n";
+	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
